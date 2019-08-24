74beea87c5cba3721f4e86cf08ee116c17be6b07
Vladimir Makarevich <makarevich.dev@outlook.com>
2019-08-25 01:36:05 +0300

    add parser files

--- SrtParser/Program.cs
+++ SrtParser/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace SrtParser
 {
@@ -6,7 +10,46 @@ namespace SrtParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"d:\torrents\The Science of Meditation (Русские субтитры)\05. The Science of Emotion and Social-Emotional Learning";
+            
+            string[] files = System.IO.Directory.GetFiles(path, "*.srt");
+
+            foreach (var filePath in files)
+            {
+                // This text is added only once to the file.
+                if (File.Exists(filePath))
+                {
+                    // Read Rows File
+                    string[] readText = File.ReadAllLines(filePath, Encoding.UTF8);
+                    var testWitoutEmpties = readText.Where(s => !string.IsNullOrEmpty(s)).ToList();
+                    var count = testWitoutEmpties.Count();
+                    var newText = new List<string>();
+                    foreach (string s in testWitoutEmpties)
+                    {
+                        Console.WriteLine(s);
+                        Int32 intRow;
+                        if (int.TryParse(s, out intRow))
+                        {
+                            newText.Add(s);
+                            continue;
+                        }
+
+                        if (s.StartsWith("00:"))
+                        {
+                            newText.Add(s);
+                            continue;
+                        }
+
+                        newText.Add(s);
+                        newText.Add(string.Empty);
+                        newText.Add(string.Empty);
+                    }
+
+                    var newFullPath = filePath.Replace(".srt", " (1).srt");
+
+                    File.WriteAllLines(newFullPath, newText, Encoding.UTF8);
+                }
+            }
         }
     }
 }
